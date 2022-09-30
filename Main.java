d35664b8909361a2fa97f76084d52c3f97d492d2
HAMMED007 <104014722+HAMMED007@users.noreply.github.com>
2022-09-30 20:49:23 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,34 +1,63 @@
-import java.util.List;
+import java.math.BigInteger;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
-
-    }
-
-
-    public class BinaryArrayToNumber{
-        public static int ConvertBinaryArrayToInt(List<Integer> binary){
+        System.out.println(Solution.zeros(10));
 
 
 
 
 
-            StringBuilder sb = new StringBuilder ();
-            for (int i = 0; i < binary.size(); i++) {
-                sb.append(binary.get(i));
-            }
 
-            String myString = String.valueOf(sb);
-
-            int decimal=Integer.parseInt(myString,2);
-
-
-            return decimal;
         }
 
+    public class Solution {
+        public static int zeros(int n) {
 
+
+
+
+            int zero_count = 0;
+            BigInteger factorial = BigInteger.ONE;
+            for (int k = n; k > 0; k--) {
+                factorial = factorial.multiply(BigInteger.valueOf(k));
+            }
+
+      
+            String string = String.valueOf(factorial);
+
+
+
+            //////////////////////////
+            StringBuilder  sb =new  StringBuilder();
+
+            char[] nnn = string.toCharArray();
+            for (int j = nnn.length-1; j > 0 ; j--) {
+                sb.append(nnn[j]);
+
+
+            }
+            for (int k = 0; k < sb.length(); k++) {
+
+                if(sb.charAt(k) == '0'){
+                    zero_count++;
+
+                }else if (sb.charAt(k) != '0'){
+                    break;
+                }
+
+
+
+
+
+            }
+
+            return zero_count;
+        }
     }
+
+
 
 }
